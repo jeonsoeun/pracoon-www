aaa7858b97ac3b6144797549bea6cf781ce1b3fb
Soeun Jun <jse525@neowiz.com>
2022-03-21 18:37:44 +0900

    prettier 적용

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
-  import {Router, Link, Route} from 'svelte-routing';
-  import Home from './routes/Home.svelte';
-  import Editor from './routes/NoticeEditor.svelte'
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./routes/Home.svelte";
+  import Editor from "./routes/NoticeEditor.svelte";
+  import NotFound from "./routes/NotFound.svelte";
 </script>
 
 <Router>
-<div class="app">
-  <div class="links">
-    <Link to="/">Home</Link>
-    <Link to="/editor">Editor</Link>
-  </div>
-  <div class="container">
-    <Route path="/editor" component="{Editor}"/>
-    <Route path="/" component="{Home}"/>
+  <div class="app">
+    <div class="links">
+      <Link to="/">Home</Link>
+      <Link to="/editor">Editor</Link>
+    </div>
+    <div class="container">
+      <Route path="/editor" component={Editor} />
+      <Route path="/" component={Home} />
+      <Route path="*" component={NotFound} />
     </div>
   </div>
 </Router>
