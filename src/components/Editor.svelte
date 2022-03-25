ce4430b0db6bc0d91343279f30c2389c3221433e
Soeun Jun <jse525@neowiz.com>
2022-03-25 14:12:28 +0900

    에디터 건드려보는중

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -3,15 +3,16 @@
   import { onMount } from "svelte";
   export let id:string = '';
   export let htmlText:string = '';
-
+  
   onMount(async () => {
     const quill = new Quill(`#${id}`, {
       modules: {
         toolbar: [
           [{ header: [1, 2, 3, false] }],
           // [{ font: ['Poppins', 'Noto Sans'] }],
-          [{ size: ['10', '11', '12'] }],
-          [{color: ['#000', '#e80000', '#00b81c', '#008cff', '#fcc603']}],
+          // [{ size: ['10', '11', '12'] }],
+          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+          [{color: [/* '#000', '#e80000', '#00b81c', '#008cff', '#fcc603' */]}],
           [{background: ['#fff', '#ffb3b3', '#7dffb1', '#70deff', '#feffa6']}],
           ["bold", "italic", "underline", "strike"],
           ["link"/*, "code-block"*/],
@@ -20,6 +21,8 @@
       placeholder: "내용을 적어주세요",
       theme: "snow",
     });
+    
+    console.log(quill.getModule('toolbar'))
   });
 </script>
 
