15380e4a610d9d9c3c6578255c56dc17dceb6128
Soeun Jun <jse525@neowiz.com>
2022-03-25 10:47:04 +0900

    캔들스틱 데이터양식 확인

--- src/components/CandlestickChart.svelte
+++ src/components/CandlestickChart.svelte
@@ -5,25 +5,25 @@
 
   onMount(() => {
     const chart = createChart(id);
-    const candlestickChart = chart.addAreaSeries();
+    const candlestickChart = chart.addCandlestickSeries();
     candlestickChart.setData([
-      { time: "2018-12-22", value: 32.51 },
-      { time: "2018-12-23", value: 31.11 },
-      { time: "2018-12-24", value: 27.02 },
-      { time: "2018-12-25", value: 27.32 },
-      { time: "2018-12-26", value: 25.17 },
-      { time: "2018-12-27", value: 28.89 },
-      { time: "2018-12-28", value: 25.46 },
-      { time: "2018-12-29", value: 23.92 },
-      { time: "2018-12-30", value: 22.68 },
-      { time: "2019-01-01", value: 27.67 },
-      { time: "2019-01-02", value: 28.67 },
-      { time: "2019-01-03", value: 22.67 },
-      { time: "2019-01-04", value: 21.67 },
-      { time: "2019-01-05", value: 20.67 },
-      { time: "2019-01-06", value: 20.67 },
-      { time: "2019-01-07", value: 22.67 },
-      { time: "2019-01-08", value: 29.67 },
+      { time: "2018-12-22", open: 32.51, close: 29.67, high: 37, low: 20},
+      { time: "2018-12-23", open: 31.11, close: 32.51, high: 33, low: 20},
+      { time: "2018-12-24", open: 27.02, close: 31.11, high: 32, low: 20},
+      { time: "2018-12-25", open: 27.32, close: 27.02, high: 28, low: 20},
+      { time: "2018-12-26", open: 25.17, close: 27.32, high: 28, low: 20},
+      { time: "2018-12-27", open: 28.89, close: 25.17, high: 29, low: 20},
+      { time: "2018-12-28", open: 25.46, close: 28.89, high: 29, low: 20},
+      { time: "2018-12-29", open: 23.92, close: 25.46, high: 26, low: 20},
+      { time: "2018-12-30", open: 22.68, close: 23.92, high: 24, low: 20},
+      { time: "2019-01-01", open: 27.67, close: 22.68, high: 29, low: 20},
+      { time: "2019-01-02", open: 28.67, close: 27.67, high: 30, low: 20},
+      { time: "2019-01-03", open: 22.67, close: 28.67, high: 30, low: 20},
+      { time: "2019-01-04", open: 21.67, close: 22.67, high: 24, low: 20},
+      { time: "2019-01-05", open: 20.67, close: 21.67, high: 22, low: 20},
+      { time: "2019-01-06", open: 20.67, close: 20.67, high: 21, low: 20},
+      { time: "2019-01-07", open: 22.67, close: 20.67, high: 23, low: 20},
+      { time: "2019-01-08", open: 29.67, close: 22.67, high: 31, low: 20},
     ]);
   });
 </script>
