fffaa55fb016b17f22d0232c54177794c6e1c197
Soeun Jun <jse525@neowiz.com>
2022-03-28 11:30:02 +0900

    에디터 custom font size 리스트 추가 및 설정 가능 확인. 구글 docs에서 복붙시 반영되느것도 확인.

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -1,34 +1,60 @@
 <script lang="ts">
   import Quill from "quill";
   import { onMount } from "svelte";
-  export let id:string = '';
-  export let htmlText:string = '';
-  
+  export let id: string = "";
+  export let htmlText: string = "";
+
   onMount(async () => {
+    // font size customize
+    const Size = Quill.import("attributors/style/size");
+    console.log(Size);
+    const sizelist = [];
+    for(let fz = 10; fz <= 32; ++fz) {
+      sizelist.push(`${fz}pt`); // pt로 해야 google docs에서 복사시 반영된다.
+    }
+    Size.whitelist = sizelist;
+    Quill.register(Size, true);
+    // 에디터 생성
     const quill = new Quill(`#${id}`, {
       modules: {
         toolbar: [
           [{ header: [1, 2, 3, false] }],
-          // [{ font: ['Poppins', 'Noto Sans'] }],
-          // [{ size: ['10', '11', '12'] }],
-          [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-          [{color: [/* '#000', '#e80000', '#00b81c', '#008cff', '#fcc603' */]}],
-          [{background: ['#fff', '#ffb3b3', '#7dffb1', '#70deff', '#feffa6']}],
+          [{ size: sizelist }],
+          [{ list: "ordered" }, { list: "bullet" }],
+          [
+            {
+              color: [
+                /* '#000', '#e80000', '#00b81c', '#008cff', '#fcc603' */
+              ],
+            },
+          ],
+          [
+            {
+              background: ["#fff", "#ffb3b3", "#7dffb1", "#70deff", "#feffa6"],
+            },
+          ],
           ["bold", "italic", "underline", "strike"],
-          ["link"/*, "code-block"*/],
+          ["link" /*, "code-block"*/],
         ],
       },
       placeholder: "내용을 적어주세요",
       theme: "snow",
     });
-    
-    console.log(quill.getModule('toolbar'))
   });
 </script>
 
 <div class="editor">
-  <div id={id} />
+  <div {id} />
 </div>
 
 <style lang="scss">
+  :global(.ql-container) {
+    font-size: inherit !important;
+  }
+  @for $i from 10 to 33  {
+    :global(.ql-snow .ql-picker.ql-size .ql-picker-item[data-value="#{$i}pt"]::before){
+      content: "#{$i}";
+      font-size: #{$i}pt !important;
+    }
+  }
 </style>
