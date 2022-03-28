219a7a4212218ebc9f26b5d98f29b16cc3b4d690
Soeun Jun <jse525@neowiz.com>
2022-03-28 14:55:54 +0900

    에디터 포커스 올라간 위치의 글씨크기 보여주도록 스타일 추가

--- src/components/Editor.svelte
+++ src/components/Editor.svelte
@@ -9,7 +9,7 @@
     const Size = Quill.import("attributors/style/size");
     console.log(Size);
     const sizelist = [];
-    for(let fz = 10; fz <= 32; ++fz) {
+    for (let fz = 10; fz <= 32; ++fz) {
       sizelist.push(`${fz}pt`); // pt로 해야 google docs에서 복사시 반영된다.
     }
     Size.whitelist = sizelist;
@@ -21,20 +21,10 @@
           [{ header: [1, 2, 3, false] }],
           [{ size: sizelist }],
           [{ list: "ordered" }, { list: "bullet" }],
-          [
-            {
-              color: [
-                /* '#000', '#e80000', '#00b81c', '#008cff', '#fcc603' */
-              ],
-            },
-          ],
-          [
-            {
-              background: ["#fff", "#ffb3b3", "#7dffb1", "#70deff", "#feffa6"],
-            },
-          ],
+          [{ color: [] }],
+          [{ background: [] }],
           ["bold", "italic", "underline", "strike"],
-          ["link" /*, "code-block"*/],
+          ["link" /*, "code-block"*/, "clipboard"],
         ],
       },
       placeholder: "내용을 적어주세요",
@@ -51,10 +41,15 @@
   :global(.ql-container) {
     font-size: inherit !important;
   }
-  @for $i from 10 to 33  {
-    :global(.ql-snow .ql-picker.ql-size .ql-picker-item[data-value="#{$i}pt"]::before){
+  @for $i from 10 to 33 {
+    :global(.ql-snow .ql-picker.ql-size .ql-picker-item[data-value="#{$i}pt"]::before) {
       content: "#{$i}";
       font-size: #{$i}pt !important;
+    }
+  }
+  @for $i from 10 to 33 {
+  :global(.ql-snow .ql-picker.ql-size .ql-picker-label[data-value="#{$i}pt"]::before) {
+      content: "#{$i}";
     }
   }
 </style>
